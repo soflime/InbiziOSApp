f9b36af97c187061ecf32b7e15c9c53a06a01157
virendragit <virendragupta@Virendras-MacBook-Pro.local>
2019-11-25 08:16:37 +0530

    added enqiury page

---
+++ InBizApp/InBizDemo/EnquiryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EnquiryViewController.swift
+//  InBizDemo
+//
+//  Created by Virendra Gupta on 25/11/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class EnquiryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
