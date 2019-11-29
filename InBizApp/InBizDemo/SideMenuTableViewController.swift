b78d61f3a851f8d84c5d92da5dad7e84ed0d579d
Virendra Gupta <virendragupta7@gmail.com>
2019-11-29 11:58:59 +0530

    post logout header issue fixed

---
+++ InBizApp/InBizDemo/SideMenuTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SideMenuTableViewController.swift
+//  SideMenu
+//
+//  Created by Jon Kent on 4/5/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import SideMenu
+
+class SideMenuTableViewController: UITableViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // refresh cell blur effect in case it changed
+        tableView.reloadData()
+        
+//        guard let menu = navigationController as? SideMenuNavigationController, menu.blurEffectStyle == nil else {
+//            return
+//        }
+        
+        // Set up a cool background image for demo purposes
+//        let imageView = UIImageView(image: #imageLiteral(resourceName: "saturn"))
+//        imageView.contentMode = .scaleAspectFit
+//        imageView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+//        tableView.backgroundView = imageView
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath) as! UITableViewVibrantCell
+
+        if let menu = navigationController as? SideMenuNavigationController {
+            cell.blurEffectStyle = menu.blurEffectStyle
+        }
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("selected row \(IndexPath.self)")
+        
+        if indexPath.row == 2 {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "Login") as! LoginViewController
+            //            self.present(nextViewController, animated:true, completion:nil)
+            self.show(nextViewController, sender: self)
+        }
+    }
+    
+}
